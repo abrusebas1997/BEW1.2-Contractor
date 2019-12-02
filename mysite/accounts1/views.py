93d1a7623dbaf5880b001fe1a9d33f8fc47efa08
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-12-02 12:42:48 -0800

    adding proposal

---
+++ mysite/accounts1/views.py
@@ -0,0 +1,17 @@
+# from django.shortcuts import render
+# from accounts.forms import UserCreationForm
+# from django.urls import reverse_lazy
+# from django.views.generic import CreateView
+#
+#
+# class SignUpView(CreateView):
+#     form_class = UserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'registration/signup.html'
+# # Create your views here.
+
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
---
+++ proposal.md
@@ -0,0 +1,3 @@
+I'm going to create a website that helps people struggling in hunger with restaurant's leftovers.
+How it works?
+There's going to be two type of accounts: 1)For restaurants to request a pick up when they have leftovers and post photos of the food, 2)For costumers to separate their meals. insert their location and give feedback
